ab1817e8c6297fae4675f1e6472ba736824fcb24
somnath-dapl <somnath@digitalaptech.com>
2018-09-06 17:58:00 +0530

    changelog - SVG support added

--- App.js
+++ App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Container, Content, Button, Icon } from 'native-base';
+import Image from 'react-native-remote-svg';
 
 export default class App extends Component {
   render() {
     return (
       <Container>
-        <Content style={{ flex: 1, flexDirection: 'row', justifyContent: 'center', alignContent: 'center', alignItems: 'center' }}>
-          <Button icon transparent>
-            <Icon type="FontAwesome" name="google" />
-          </Button>
-        </Content>
+        <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'center' }}>
+          <View style={{ height: '60%', width: '100%' }}></View>
+          <View style={{ height: '40%', width: '100%', flexDirection: 'row', justifyContent: 'center' }}>
+            <Button icon transparent>
+              <Image source={require('./assets/icons/search.svg')} style={{ width: 64, height: 64 }} />
+            </Button>
+          </View>
+        </View>
       </Container>
     );
   }
